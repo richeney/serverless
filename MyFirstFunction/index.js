2483bf9bff6cfd9cc7f688e70bc15d455062e7c5
Richard Cheney <richeney@microsoft.com>
2020-01-13 10:47:53 +0000

    Code change 2

--- MyFirstFunction/index.js
+++ MyFirstFunction/index.js
@@ -1,6 +1,7 @@
 module.exports = async function (context, req) {
     context.log('Generating Nonsense...');
+    let now =new Date();
     context.res = {
-        body: "Bibble wibble wobble"
+        body: "The date & time now is: " + now.toISOString() + "T" + now.toTimeString()
     };
 };
